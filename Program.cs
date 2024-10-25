c0134af3d16f9c3a3458be0b6c937b225ffeeb9c
vecseizsolt <vecseizs@kkszki.hu>
2024-10-25 10:53:18 +0200

    A projekt 'a' és 'b' feladata elkészült.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _241025_Szemazonosito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string szemazon = "";
+            Console.WriteLine( "Adja meg a személyazonosító jelének első 10 jegyét: " );
+            szemazon = Console.ReadLine();
+            if (szemazon[0] % 2 == 0) Console.WriteLine("A megadott személyazonosító jel alaplán a személy neme nő.");
+            else Console.WriteLine("A megadott személyazonosító jel alaplán a személy neme férfi.");
+        }
+    }
+}
