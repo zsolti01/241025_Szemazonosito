8d7fccfcd46a91ec72eb82aedebbb99e9a04725e
vecseizsolt <vecseizs@kkszki.hu>
2024-10-25 11:50:40 +0200

    Az 'e' és 'f' feladat is elkészült.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -12,12 +13,27 @@ namespace _241025_Szemazonosito
         static void Main(string[] args)
         {
             string szemazon = "";
+            string szemazon2 = "";
             Console.WriteLine( "Adja meg a személyazonosító jelének első 10 jegyét: " );
             szemazon = Console.ReadLine();
+            
             Console.WriteLine();
+            
             if (szemazon[0] % 2 == 0) Console.WriteLine("A megadott személyazonosító jel alaplán a személy neme nő.");
             else Console.WriteLine("A megadott személyazonosító jel alaplán a személy neme férfi.");
             Console.WriteLine("A megadott személyazonosító jel személyének születési időpontja: 19{0}{1}. {2}{3}. {4}{5}.", szemazon[1], szemazon[2], szemazon[3], szemazon[4], szemazon[5], szemazon[6]);
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Adja meg a második személyazonosító jelének első 10 jegyét is: ");
+            szemazon2 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            if (szemazon[1] > szemazon2[1]) Console.WriteLine("Az első személyazonosító jel személye az idősebb.");
+            else Console.WriteLine("A második személyazonosító jel személye az idősebb.");
+
+            Console.ReadKey();
         }
     }
 }
