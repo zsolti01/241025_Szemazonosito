0c2132e68e9eb0f0ff162eacb4c3337b3f7448b7
vecseizsolt <vecseizs@kkszki.hu>
2024-10-25 10:59:52 +0200

    A 'c' feladat is elkészült.

--- Program.cs
+++ Program.cs
@@ -14,8 +14,10 @@ namespace _241025_Szemazonosito
             string szemazon = "";
             Console.WriteLine( "Adja meg a személyazonosító jelének első 10 jegyét: " );
             szemazon = Console.ReadLine();
+            Console.WriteLine();
             if (szemazon[0] % 2 == 0) Console.WriteLine("A megadott személyazonosító jel alaplán a személy neme nő.");
             else Console.WriteLine("A megadott személyazonosító jel alaplán a személy neme férfi.");
+            Console.WriteLine("A megadott személyazonosító jel személyének születési időpontja: 19{0}{1}. {2}{3}. {4}{5}.", szemazon[1], szemazon[2], szemazon[3], szemazon[4], szemazon[5], szemazon[6]);
         }
     }
 }
